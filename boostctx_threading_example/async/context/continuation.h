24c58b63028782ba90125091c80f0ac9cc2d7aa9
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2023-06-05 17:49:45 +0300

    Add `context_function` concept.
    Improve documentation.

--- boostctx_threading_example/async/context/continuation.h
+++ boostctx_threading_example/async/context/continuation.h
@@ -23,22 +23,34 @@ boost::context::continuation to_boost_continuation(context_handle h) {
 }
 } // namespace detail
 
-// TODO: concept for (context_handle) -> context_handle
+//! Concept for the context function types.
+//! This matches all invocables with the signature `(context_handle) -> context_handle`.
+template <typename F>
+concept context_function = requires(F&& f, context_handle continuation) {
+  { std::invoke(std::forward<F>(f), continuation) } -> std::same_as<context_handle>;
+};
 
-template <typename F> context_handle callcc(F&& f) {
+//! Call with current continuation.
+//! Takes the context of the code immediatelly following this function call, and passes it to the
+//! given context function. The given function is executed in a new stack context. We can suspend
+//! the context and resume other context, or the given context.
+inline context_handle callcc(context_function auto&& f) {
   (void)profiling::zone{CURRENT_LOCATION()};
   using bcont = boost::context::continuation;
   auto r = boost::context::callcc([f = std::move(f)](bcont&& c) -> bcont {
-    auto result = f(detail::from_boost_continuation(c));
+    auto result = std::invoke(std::move(f), detail::from_boost_continuation(c));
     return detail::to_boost_continuation(result);
   });
   return detail::from_boost_continuation(r);
 }
 
-inline context_handle resume(context_handle cont) {
+//! Resumes the given continuation.
+//! The current execution is interrupted, and the program continues from the given continuation
+//! point. Returns the context that has been suspended.
+inline context_handle resume(context_handle continuation) {
   (void)profiling::zone{CURRENT_LOCATION()};
-  assert(cont);
-  return boost::context::detail::jump_fcontext(cont, nullptr).fctx;
+  assert(continuation);
+  return boost::context::detail::jump_fcontext(continuation, nullptr).fctx;
 }
 
 } // namespace context
